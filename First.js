0b206af0c61da27f0f38e084dc44fdaa69d35320
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2023-08-22 01:06:06 +0530

    Learning Java script

---
+++ First.js
@@ -0,0 +1,23 @@
+console.log("Hello World");
+console.warn("Might be error")
+function add(a,b){
+    return a+b;
+
+}
+let c=add(5,6)
+console.log(c);
+
+var a="Ayush"
+let b="Ayu"
+const d="A"
+// d="ww"
+b=122111
+x=2112
+console.log(x);
+console.log(d);
+
+{
+    const d ="demon"
+    console.log(d);
+}
+console.log(d);
