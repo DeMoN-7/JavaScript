48e795cd9156dc4c28e20dd9006a582b71806b64
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2023-08-22 01:56:13 +0530

    commit

---
+++ PracticeSet.js
@@ -0,0 +1,37 @@
+// ............Q1.........
+let a="dolo"
+let b=600
+c=a+b
+console.log(c);
+// ............Q2......................
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+
+// ...............Q3..................
+
+const aa={
+    "first":"ayush"
+}
+aa["first"]=21
+console.log(aa["first"]);
+
+
+// ....................Q4.............
+
+
+aa["second"]="Aman"
+console.log(aa["second"]);
+
+// .................Q5............
+
+const dd={
+    "one":1,
+    "two":2,
+    "three":3,
+    "four":4,
+    "five":5
+}
+console.log(dd);
