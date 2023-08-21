ade4045bc70b56d0a579634d8ab06ef344accf37
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2023-08-22 01:56:31 +0530

    Learning JS

--- First.js
+++ First.js
@@ -1,23 +1,55 @@
-console.log("Hello World");
-console.warn("Might be error")
-function add(a,b){
-    return a+b;
+// console.log("Hello World");
+// console.warn("Might be error")
+// function add(a,b){
+//     return a+b;
 
+// }
+// let c=add(5,6)
+// console.log(c);
+
+// var a="Ayush"
+// let b="Ayu"
+// const d="A"
+// // d="ww"
+// b=122111
+// x=2112
+// console.log(x);
+// console.log(d);
+
+// {
+//     const d ="demon"
+//     console.log(d);
+// }
+// console.log(d);
+
+
+// ...........Variables.........
+/*
+Null
+Number
+Boolean
+Big int
+Symbols
+String
+Uundefined 
+*/
+
+let a=null
+let b=21
+let c=true
+let d=BigInt(55)
+let e=Symbol("weeww")
+let f="Ayush is Great"
+let g
+console.log(a,'\n',b,'\n',c,'\n',d,'\n',e,'\n',f,'\n',g);
+
+
+//................ Object................
+
+const obj={
+    "ayush":true,
+    "ayush1":"wwd",
+    "ayush2":12,
+    "ayush3":Symbol("we human are dying")
 }
-let c=add(5,6)
-console.log(c);
-
-var a="Ayush"
-let b="Ayu"
-const d="A"
-// d="ww"
-b=122111
-x=2112
-console.log(x);
-console.log(d);
-
-{
-    const d ="demon"
-    console.log(d);
-}
-console.log(d);
+console.log(obj["ayush1"]);
